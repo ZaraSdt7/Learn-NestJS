1bb25f91f233a5a91c3fe3fddf93878d15f33381
zahra <zahra.st7373@gmail.com>
2024-02-05 11:43:40 +0330

    start...

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CommentSchema } from './schema/comment.schema';
+import { UsersModule } from 'src/users/users.module';
+import { BlogModule } from 'src/blog/blog.module';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+    UsersModule,
+    BlogModule,
+  ],
   controllers: [CommentsController],
   providers: [CommentsService],
 })
--- src/comments/dto/create-comment.dto.ts
+++ src/comments/dto/create-comment.dto.ts
@@ -1,1 +1,11 @@
-export class CreateCommentDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateCommentDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: 'Frist comment....',
+    description: ' user Comment....',
+  })
+  text: string;
+}
